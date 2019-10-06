2b357a970d4d2e95db1dbaac3f5c888a8108c84e
Reto Hablützel <rethab@protonmail.ch>
2019-10-06 21:25:10 +0200

    use structopt

--- kvs/src/bin/kvs.rs
+++ kvs/src/bin/kvs.rs
@@ -1,47 +1,28 @@
-extern crate clap;
 extern crate kvs;
 
-use clap::{App, Arg, SubCommand};
 use kvs::KvStore;
-use std::process;
+use structopt::StructOpt;
 
 fn main() {
-    let matches = App::new(env!("CARGO_PKG_NAME"))
-        .version(env!("CARGO_PKG_VERSION"))
-        .author(env!("CARGO_PKG_AUTHORS"))
-        .about(env!("CARGO_PKG_DESCRIPTION"))
-        .subcommand(
-            SubCommand::with_name("set")
-                .about("sets a value")
-                .arg(Arg::with_name("key").value_name("KEY").required(true))
-                .arg(Arg::with_name("value").value_name("VALUE").required(true)),
-        )
-        .subcommand(
-            SubCommand::with_name("get")
-                .about("gets a value")
-                .arg(Arg::with_name("key").value_name("KEY").required(true)),
-        )
-        .subcommand(
-            SubCommand::with_name("rm")
-                .about("removes a value")
-                .arg(Arg::with_name("key").value_name("KEY").required(true)),
-        )
-        .get_matches();
-
+    let opt = Cmd::from_args();
     let mut kv = KvStore::new();
-    if let Some(set_cmd) = matches.subcommand_matches("set") {
-        let key = set_cmd.value_of("key").unwrap().to_owned();
-        let value = set_cmd.value_of("value").unwrap().to_owned();
-        panic!("unimplemented");
-    } else if let Some(get_cmd) = matches.subcommand_matches("get") {
-        let key = get_cmd.value_of("key").unwrap().to_owned();
-        panic!("unimplemented");
-    } else if let Some(rm_cmd) = matches.subcommand_matches("rm") {
-        let key = rm_cmd.value_of("key").unwrap().to_owned();
-        panic!("unimplemented");
-    } else if !matches.is_present("version") {
-        eprintln!("missing command");
-        println!("{}", matches.usage());
-        process::exit(1);
+
+    match opt {
+        Cmd::Set { key, value } => panic!("unimplemented"),
+        Cmd::Get { key } => panic!("unimplemented"),
+        Cmd::Remove { key } => panic!("unimplemented"),
     }
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(about = "A simple key value store")]
+enum Cmd {
+    #[structopt(name = "set", about = "Puts a value into the store")]
+    Set { key: String, value: String },
+
+    #[structopt(name = "get", about = "Retrieves a value from the store")]
+    Get { key: String },
+
+    #[structopt(name = "rm", about = "Removes a value from the store")]
+    Remove { key: String },
 }
